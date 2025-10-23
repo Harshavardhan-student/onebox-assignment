6ba7c08560e24508b5405b578a65ae0d07ec46be
Harsha Kota <harshavardhan94935@gmail.com>
2025-10-23 14:57:02 +0530

    Configure for Netlify deployment

---
+++ onebox/backend/netlify/functions/api.ts
@@ -0,0 +1,12 @@
+import { Handler } from '@netlify/functions';
+import express from 'express';
+import serverless from 'serverless-http';
+import cors from 'cors';
+import emailsRouter from '../../src/routes/emails';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api/emails', emailsRouter);
+
+export const handler: Handler = serverless(app);
